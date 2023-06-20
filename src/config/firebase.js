0884b5913aa2518f16dce619a17a1642596e6548
Rifat996 <98282200+Rifat996@users.noreply.github.com>
2023-06-20 08:39:57 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBd71oD5GW1pREX0Fhbjkqt7oojgQwiLDo",
-  authDomain: "kinlab-project.firebaseapp.com",
-  projectId: "kinlab-project",
-  storageBucket: "kinlab-project.appspot.com",
-  messagingSenderId: "561753897835",
-  appId: "1:561753897835:web:7881568dd4aba8a4ac6a2e",
-  measurementId: "G-7G14KNX66F"
+ X
 };
 
 
